9be5fca43351303a58b317e0c4aed75e19501880
Stewart ilondanga <stilondanga@gmail.com>
2020-06-21 14:57:02 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,37 @@
 var navigate = (function() {
-	$('.dd').toggle();
-	$('.dd_btn').click(function() {
-		var dataName = $(this).attr('data-name');
-		$('.dd').hide();
-		$('.' + dataName).toggle();
-	});
+  $('.dd').toggle();
+  $('.dd_btn').click(function() {
+    var dataName = $(this).attr('data-name');
+    $('.dd').hide();
+    $('.' + dataName).toggle();
+  });
 })();
+
+const __checkbox = document.getElementsByTagName('input')[0];
+
+const __label = document.createElement('LABEL');
+__label.className = 'switch';
+__label.id = 'alien';
+
+const __span = document.createElement('SPAN');
+__span.className = 'slider';
+
+__label.appendChild(__checkbox);
+__label.appendChild(__span);
+
+document.body.appendChild(__label);
+
+// Toggle event
+document.getElementsByTagName('input')[0].onclick = () => {
+  checkChecked();
+};
+
+function checkChecked() {
+  if (document.getElementsByTagName('input')[0].checked) {
+    document.body.className = 'shabley';
+    document.getElementById('alien').className = 'switch hielene';
+  } else {
+    document.body.className = '';
+    document.getElementById('alien').className = 'switch';
+  }
+};
